cbab6bc9317d8d26136ebb2d6a789b74e1fdd727
ernesto <bossi.ernestog@gmail.com>
2014-03-02 20:18:25 -0300

    went 0.0.8

--- django_bootstrap3view/setup.py
+++ django_bootstrap3view/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 #define variables for setup.py
 __author__ = 'Ernesto Bossi'
-__version__ = "0.0.7"
+__version__ = "0.0.8"
 
 __dir__ = os.path.dirname(os.path.abspath(__file__))
 templates_dir = os.path.join(__dir__, "django_bootstrap3view_app", "management", "commands", "templates")
@@ -12,8 +12,6 @@ templates_files = [os.path.join(templates_dir, file) for file in os.listdir(temp
 
 media_dir = os.path.join(__dir__, "django_bootstrap3view_app", "management", "commands", "templates")
 media_files = [os.path.join(media_dir, file) for file in os.listdir(media_dir)]
-
-misc_files = [os.path.join(os.pardir, file) for file in ["LICENSE", "changelog.md"]]
 
 setup(
     name='django-classview-bootstrap3',
@@ -32,7 +30,7 @@ setup(
     ],
     packages=find_packages(exclude=["django_bootstrap3view"]),
     data_files=[
-        (templates_dir, templates_files), ('', misc_files), (media_dir, media_files)
+        (templates_dir, templates_files), (media_dir, media_files)
     ],
     include_package_data=True,
     install_requires=[
